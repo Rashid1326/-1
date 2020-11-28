efbc895477dc226e50871183daa1c814ecd9a61c
Rashid1326 <75166863+Rashid1326@users.noreply.github.com>
2020-11-28 15:21:28 +0300

    Add files via upload

---
+++ лаба1/лаба1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace лаба1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Лабораторная работа №1");
+            Console.WriteLine("РАЗРАБОТКА КОНСОЛЬНОГО ПРИЛОЖЕНИЯ");
+            Console.WriteLine("Шихмагомедов Абдурашид Курбанмагомедович");
+            Console.WriteLine("ИНС-б-о-19-1");
+            Console.WriteLine("13.02.2001");
+            Console.WriteLine("Ставрополь");
+            Console.WriteLine("Лбиммый предмет астрономия ");
+            Console.WriteLine("Хобби нету");
+
+
+
+
+
+
+
+
+            int f = 5;
+            int y = 3;
+            int Zze;
+            Zze = (35 / f) + y * f - ((f + y) / 4);
+
+
+            
+            Console.WriteLine(Zze);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
